58ec8e56e361598f7714132ea5f8e566c94e4eef
Wahab Janjua <janjuamo@gmail.com>
2018-04-26 23:50:13 -0500

    Menu Part 2
    Added functionality to choose and only display selected section.

---
+++ js/Exam2problem.js
@@ -0,0 +1,53 @@
+//Menu
+function MenuChoice()
+{
+    
+    if (document.getElementById("menu").value == "Get category list")
+    {
+        document.getElementById("section1").style.visibility = "visible";
+        document.getElementById("section2").style.visibility = "hidden";
+        document.getElementById("section3").style.visibility = "hidden";
+        document.getElementById("section4").style.visibility = "hidden";
+        document.getElementById("section5").style.visibility = "hidden";
+    }
+    else if (document.getElementById("menu").value == "Add product category to database")
+    {
+        document.getElementById("section1").style.visibility = "hidden";
+        document.getElementById("section2").style.visibility = "visible";
+        document.getElementById("section3").style.visibility = "hidden";
+        document.getElementById("section4").style.visibility = "hidden";
+        document.getElementById("section5").style.visibility = "hidden";
+    }
+    else if (document.getElementById("menu").value == "Change category description in database")
+    {
+        document.getElementById("section1").style.visibility = "hidden";
+        document.getElementById("section2").style.visibility = "hidden";
+        document.getElementById("section3").style.visibility = "visible";
+        document.getElementById("section4").style.visibility = "hidden";
+        document.getElementById("section5").style.visibility = "hidden";
+    }
+    else if (document.getElementById("menu").value == "Delete a category from database")
+    {
+        document.getElementById("section1").style.visibility = "hidden";
+        document.getElementById("section2").style.visibility = "hidden";
+        document.getElementById("section3").style.visibility = "hidden";
+        document.getElementById("section4").style.visibility = "visible";
+        document.getElementById("section5").style.visibility = "hidden";
+    }
+    else if (document.getElementById("menu").value == "About")
+    {
+        document.getElementById("section1").style.visibility = "hidden";
+        document.getElementById("section2").style.visibility = "hidden";
+        document.getElementById("section3").style.visibility = "hidden";
+        document.getElementById("section4").style.visibility = "hidden";
+        document.getElementById("section5").style.visibility = "visible";
+    }
+    else
+    {
+        document.getElementById("section1").style.visibility = "hidden";
+        document.getElementById("section2").style.visibility = "hidden";
+        document.getElementById("section3").style.visibility = "hidden";
+        document.getElementById("section4").style.visibility = "hidden";
+        document.getElementById("section5").style.visibility = "hidden";
+    }
+}
